d332def8b471809ec3959934a6669aea1700a1a7
mahmoud <mahmoud.elbatal2019@gmail.com>
2026-04-15 19:43:29 +0200

    Edited and comments added

--- bin/bank_management_system.dart
+++ bin/bank_management_system.dart
@@ -1,62 +1,100 @@
-class Account{
+// class Account with 2 Mixins(Validation, LimitChecker) and implements 1 interface(AccountOperation)
+class Account with Validation, LimitChecker implements AccountOperations{
   int accNum;
   double _balance;
-  final List<Transaction> _transactions = [];
-  Account(this.accNum, double balance) : _balance = balance;
+  // A final list of objects from a class(Transaction)
+  final List<Transaction> _transactions = [];  // this is a Composition because logically "an account is composed of transactions"
+  Account(this.accNum, this._balance); //constructor
+
+  @override // the implementation of deposit method from AccountOperations interface
   void deposit(double amount){
     _balance += amount;
-    _transactions.add(Transaction(_transactions.length+1, amount));
+    _transactions.add(Transaction(_transactions.length+1, amount, TransactionType.deposit));
   }
+  @override // the implementation of withdraw method from AccountOperations interface
   void withdraw(double amount){
-    if (amount <= _balance){
+    // isValidAmount and canWithdraw are methods of Validation and LimitChecker interfaces
+    if(isValidAmount(amount) && canWithdraw(_balance, amount)){
       _balance -= amount;
-      _transactions.add(Transaction(_transactions.length+1, -amount));
+      //adding an object(Transaction) to transactions objects
+      //Transaction(transactionId, amount, TransactionType)
+      _transactions.add(Transaction(_transactions.length+1, -amount, TransactionType.withdraw));
     }
     else{
       print("Enter a valid amount");
     }
   }
-  double get balance => _balance;
-  List<Transaction> get transactions => _transactions;
+  double get balance => _balance; //using Getter to access the balance
+  List<Transaction> get transactions => _transactions; // using Getter to access transactions
 }
+
+// Savings Account has interest rate
 class SavingsAccount extends Account{
   double interestRate;
-  SavingsAccount(super.accNum, super.balance, this.interestRate);
+  SavingsAccount(super.accNum, super.balance, this.interestRate); //constructor
+  //method of adding interest
   void addInterest() {
     _balance += _balance * interestRate;
   }
 }
+
+// Current Account has overdraft limit
 class CurrentAccount extends Account{
   double overdraftLimit;
-  CurrentAccount(super.accNum, super.balance, this.overdraftLimit);
+  CurrentAccount(super.accNum, super.balance, this.overdraftLimit); //constructor
   @override
   void withdraw(double amount){
-    if (amount <= _balance + overdraftLimit){
+    if (isValidAmount(amount) && canWithdraw(_balance + overdraftLimit, amount)){
       _balance -= amount;
-      _transactions.add(Transaction(_transactions.length+1, -amount));
+      _transactions.add(Transaction(_transactions.length+1, -amount, TransactionType.withdraw));
     }
     else{
       print("Enter a valid amount");
     }
   }
 }
+
+
 class Customer{
   int customerId;
   String name;
-  List<Account> accounts = [];
+  List<Account> accounts = []; // This is an Association because logically "A customer has an account"
 
-  Customer(this.customerId, this.name);
-
+  Customer(this.customerId, this.name); //constructor
+  //method to add object(account)
   void addAccount(Account account){
     accounts.add(account);
   }
 }
+
+
 class Transaction{
   int transactionId;
   double amount;
-  Transaction(this.transactionId, this.amount);
+  TransactionType type;// enum
+  Transaction(this.transactionId, this.amount, this.type); //constructor
+
+  // this override is for making the object of Transaction appear in this way when printed
   @override
   String toString(){
-    return "Transaction $transactionId : $amount";
+    return "Transaction $transactionId : ${type.name} $amount";
+  }
+}
+enum TransactionType{
+  deposit,
+  withdraw
+}
+abstract class AccountOperations{
+  void deposit (double amount);
+  void withdraw (double amount);
+}
+mixin Validation{
+  bool isValidAmount(double amount){
+    return amount > 0;
+  }
+}
+mixin LimitChecker{
+  bool canWithdraw(double balance, double amount){
+    return amount <= balance;
   }
 }
--- bin/main.dart
+++ bin/main.dart
@@ -1,11 +1,14 @@
 import 'bank_management_system.dart';
 void main(){
   Customer customer1 = Customer(123, "Mahmoud");
+  print("Customer Name: ${customer1.name}");
   SavingsAccount acc1 = SavingsAccount(1, 10000, 0.1);
   customer1.addAccount(acc1);
+  print("Account Balance: ${acc1.balance}");
   acc1.deposit(10000);
+  print("deposited");
   acc1.addInterest();
   print("Account Balance: ${acc1.balance}");
-  print("Account Transaction: ${acc1.transactions}");
+  print("Account Transactions: ${acc1.transactions}");
 
 }
